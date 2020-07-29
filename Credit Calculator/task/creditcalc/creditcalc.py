c47364b95da46079f34ac105422af30e0a5aa1f1
sman01 <shreemun@gmail.com>
2020-07-29 12:37:21 +0530

    stage 2 done

--- Credit Calculator/task/creditcalc/creditcalc.py
+++ Credit Calculator/task/creditcalc/creditcalc.py
@@ -1,11 +1,22 @@
-credit_principal = 'Credit principal: 1000'
-final_output = 'The credit has been repaid!'
-first_month = 'Month 1: paid out 250'
-second_month = 'Month 2: paid out 250'
-third_month = 'Month 3: paid out 500'
-print(credit_principal)
-print(first_month)
-print(second_month)
-print(third_month)
-print(final_output)
-# write your code here
+amt = int(input("Enter the credit principal:"))
+print('''
+What do you want to calculate? 
+type "m" for the count of months, 
+type "p" for monthly payment:
+''')
+sel = input()
+if sel == 'p':
+    mon = int(input("Enter the count of months:"))
+    if amt % mon == 0:
+        print("Your monthly payment = " + str(amt/mon))
+    elif amt % mon >= 0.1:
+        rem = amt - (int(amt/mon) + 1) * (mon - 1)
+        print("Your monthly payment = "+ str((int(amt/mon) + 1)) +" with last month payment = "+ str(rem) +".")
+elif sel == 'm':
+    pay = int(input("Enter the monthly payment:"))
+    if round(amt/pay) == 1:
+         print("It takes "+str(round(amt/pay))+" month to repay the credit")
+    else:
+        print("It takes "+str(round(amt/pay))+" months to repay the credit")
+
+
